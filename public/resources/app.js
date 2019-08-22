575c700de081d03f426db801fd4e2c324498b8fc
Cameron Cruickshank <cameron_cruickshank@cooperators.ca>
2019-08-22 19:50:28 +0000

    web commits 2

--- public/resources/app.js
+++ public/resources/app.js
@@ -28,13 +28,23 @@ app.run(function($rootScope) {
 });
 
 app.config(['$routeProvider', function($routeProvider) {
-    console.log("entering Angular config");
-
-    $routeProvider.when('/home', {
-       templateUrl : baseUrl + 'home.html',
-       controller: 'HomeController'
-    })
-    .otherwise({
-        redirectTo: '/home'
-    });
+$routeProvider.when('/home', {
+templateUrl : baseUrl + 'home.html',
+controller: 'HomeController'
+})
+.when('/catalog', {
+templateUrl : baseUrl + 'catalog.html',
+controller: 'CatalogController'
+})
+.when('/item/:id', {
+templateUrl : baseUrl + 'item.html',
+controller: 'ItemController'
+})
+.when('/customer', {
+templateUrl : baseUrl + 'customer.html',
+controller: 'CustomerController'
+})
+.otherwise({
+redirectTo: '/home'
+});
 }]);
